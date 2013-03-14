a4e62f43bcd55b1279e1a5842664aec6979f2d51
pradiptg <pradiptg@ubuntu.(none)>
2013-03-14 15:29:48 -0700

    proc init

--- weenix/kernel/proc/proc.c
+++ weenix/kernel/proc/proc.c
@@ -82,6 +82,42 @@ failed:
 proc_t *
 proc_create(char *name)
 {
+        //Allocate a slab to a process
+         proc_t *new_proc_t = slab_obj_alloc(proc_allocator);
+ 
+          //check whether allocation is succesful or not
+          KASSERT(new_proc_t!=NULL);
+         
+          //Empty the content of the slab
+          memset(new_proc_t, 0, sizeof(proc_t));
+         
+          //Get the proc_id
+          new_proc_t->p_pid=_proc_getid();
+          KASSERT(new_proc_t->p_pid>=0);  
+          if(new_proc_t->p_pid==0)
+                //create INIT process
+          else if(new_proc_t->p_pid==1)
+                 proc_initproc =new_proc_t;//set the process list header to point the INIT process
+          else;
+        
+          //Assign the Process name 
+          if(strlen(name)<PROC_NAME_LEN)
+                strcpy(new_proc_t->p_comm,name);
+          else
+                strncpy(new_proc_t->p_comm,name,PROC_NAME_LEN);
+              
+          //Initialize the list containing its threads
+          list_init(&(newProcess->p_threads));
+          
+          //Initialize the list containg its childrens
+          list_init(&(newProcess->p_children));
+
+          //pOINTER TO PARENT PROCESS
+          new_proc_t->p_pproc=curproc;
+          
+          //Set Process State
+          new_proc_t->p_state=PROC_RUNNING;
+                
         NOT_YET_IMPLEMENTED("PROCS: proc_create");
         return NULL;
 }
