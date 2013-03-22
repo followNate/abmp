e2afbbf4a1753874b42943b1b5f69e888bdd293a
Pradipta Ghosh <iampradiptaghosh@gmail.com>
2013-03-21 17:56:54 -0700

    modified sched

--- weenix/kernel/proc/sched.c
+++ weenix/kernel/proc/sched.c
@@ -257,6 +257,8 @@ sched_switch(void)
                 thr1=curthr;
                 curthr=ktqueue_dequeue(&kt_runq);
                 curproc=curthr->kt_proc;
+                intr_setipl(interrupt_l);
+                dbg(DBG_SCHED,"After switch: current thread is the thread of process %s (PID= %d) \n",curproc->p_comm,curproc->p_pid);
                 context_switch(&(thr1->kt_ctx), &(curthr->kt_ctx));
          }
          else
@@ -272,12 +274,12 @@ sched_switch(void)
                 thr1=curthr;
                 curthr=ktqueue_dequeue(&kt_runq);
                 curproc=curthr->kt_proc;
-
+                intr_setipl(interrupt_l);
+                dbg(DBG_SCHED,"After switch: current thread is the thread of process %s (PID= %d) \n",curproc->p_comm,curproc->p_pid);
                 context_switch(&(thr1->kt_ctx), &(curthr->kt_ctx));
           }
            
-           intr_setipl(interrupt_l);
-           dbg(DBG_SCHED,"After switch: current thread is the thread of process %s (PID= %d) \n",curproc->p_comm,curproc->p_pid);
+           
          /* if(curthr->kt_cancelled==1)
           {
                 kthread_exit((void *)curthr->kt_retval);
