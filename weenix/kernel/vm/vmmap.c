be62d3989b5eab542c8d8dc29e736812e434a6d2
mohito83 <maggarwa@usc.edu>
2013-04-15 16:04:41 -0700

    commiting access.c

--- weenix/kernel/vm/vmmap.c
+++ weenix/kernel/vm/vmmap.c
@@ -341,7 +341,7 @@ int
 vmmap_read(vmmap_t *map, const void *vaddr, void *buf, size_t count)
 {
 	uint32_t *vfn = (uint32_t*)vaddr;
-	vmarea_t *vma = vmmap_lookup(map,vfn);
+	vmarea_t *vma = vmmap_lookup(map,*vfn);
 	KASSERT(NULL!=vma);
 	
 	if(vma){
