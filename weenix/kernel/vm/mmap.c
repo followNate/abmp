71be27b9c9ca062737c9f570a0470889f536c83c
mohito83 <maggarwa@usc.edu>
2013-04-25 18:59:34 -0700

    before revert

--- weenix/kernel/vm/mmap.c
+++ weenix/kernel/vm/mmap.c
@@ -119,8 +119,7 @@ do_mmap(void *addr, size_t len, int prot, int flags,int fd, off_t off, void **re
 		tlb_flush(address);
 		
 		/* Calling the function vmmmap_map */
-		vmarea_t **new;
-		int i = vmmap_map(curproc->p_vmmap, curproc->p_files[fd]->f_vnode, 0, 0, prot, flags, off, NULL, new);
+		int i = vmmap_map(curproc->p_vmmap, curproc->p_files[fd]->f_vnode, 0, 0, prot, flags, off, NULL, (vmarea_t**)ret);
 		return 0;
        
 		NOT_YET_IMPLEMENTED("VM: do_mmap");
